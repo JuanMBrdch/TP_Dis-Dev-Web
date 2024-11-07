a302968582ac5e5c0036ac75e971b5eef0463108
Camila Liendro <camila.liendro@gmail.com>
2024-11-07 00:22:53 -0300

    Welcome-Message

--- functions/logica.js
+++ functions/logica.js
@@ -1,3 +1,18 @@
 function sumar_carrito(){
     var suma = value1 + value2
 }
+
+
+window.onload = function() {
+    if (!sessionStorage.getItem('blockShown')) 
+    {
+        document.getElementById("welcomeBlock").style.display = "block";
+        
+        sessionStorage.setItem('blockShown', 'true');
+    }
+};
+
+function closeModal() 
+{
+    document.getElementById("welcomeBlock").style.display = "none";
+}
